d893fe2edff6588f0b5c4ac958ed8d4941d08229
Elsa Gonsiorowski (Eris) <gonsie@me.com>
2017-06-26 16:56:05 -0600

    fixing metadata (thanks @IanLee1521)

--- setup.py
+++ setup.py
@@ -4,10 +4,19 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='melodee',
+setup(
+      name='melodee',
       version='0.1',
       description='Differential Equation Compiler for HPC',
-      url='http://github.com/llnl/melodee',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python 2',
+          'Programming Language :: Python 3'
+      ],
+      url='https://github.com/llnl/melodee',
       author='Robert Clayton Blake III',
       author_email='blake14@llnl.gov',
       license='Apache-2.0',
